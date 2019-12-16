954d43eeee8f010610ba50becf36768979da696e
RobAnKo <robinkoch92@gmail.com>
2019-12-16 23:01:51 +0100

    puzzle10.2 done

--- AoC19/script10.py
+++ AoC19/script10.py
@@ -21,9 +21,9 @@ from scipy import signal
 from datetime import datetime
 import copy
 import math
-from typing import Iterable
-#os.chdir("/home/karlchen/Desktop/AdventOfCode/AoC19")
-os.chdir("/home/robinkoch/Desktop/AdventOfCode/AoC19")
+from typing import Tuple,Iterable,List
+os.chdir("/home/karlchen/Desktop/AdventOfCode/AoC19")
+#os.chdir("/home/robinkoch/Desktop/AdventOfCode/AoC19")
 
 
 
@@ -63,7 +63,7 @@ def coincident(one, two):
 
 
 
-
+'''
 inputfile = "Input_10_test2.txt"
 asteroid_map = asteroid_map_from_file(inputfile)
 positions = np.transpose(np.nonzero(asteroid_map))
@@ -103,14 +103,14 @@ maxpos = positions[np.argmax(visible_per_pos)]
     
 print(f"Solution for Puzzle10.1:\n Best is {maxpos} with {max(visible_per_pos)} asteroids detected")
 
-
+'''
 #puzzle10.2
 
 #We know from 10.1 that the station is at [31,25]
 # if we sort the direction vectors by polar coordinates (i.e. angle) we can
 # find the next asteroid to evaporate
 
-def cart_to_polar(vector: Iterable[int]) -> Iterable[int]:
+def cart_to_polar(vector: Iterable[int]) -> List[int]:
     x = vector[0]
     y = vector[1]
     z = x + 1j * y
@@ -120,38 +120,63 @@ def cart_to_polar(vector: Iterable[int]) -> Iterable[int]:
         t += 360
     return([r,t])
 
-def rotate(matrix):
-    return np.array([x for x in zip(*matrix[::-1])])
+def polar_to_cart(vector: Iterable[int]) -> List[int]:
+    r = vector[0]
+    t = np.radians(vector[1])
+    x = r * np.cos(t)
+    y = r * np.sin(t)
+    return([x,y])
 
+#def rotate(matrix):
+#    return np.array([x for x in zip(*matrix[::-1])])
+
+
+def find_new_laser_angle(angle_before,sight_vectors):
+    #find index in list of objects which is closest clockwise to old laser angle
+    l = [ast for ast in sight_vectors if ast[1]<angle_before]
+    if not l:
+        #we have no objects with angles smaller than angle before, so reset at 360
+        l = [ast for ast in sight_vectors if ast[1]<360]
+    return(l[0][1])
+
+#def rotate_point(point,shape_of_map):
+#    return((point[1],shape_of_map[1]-point[0]-1))
 
 inputfile = "Input_10.txt"
 asteroid_map = asteroid_map_from_file(inputfile)
 laser_pos = (31,25)
-rot_laser_pos = (laser_pos[1],len(asteroid_map[0])-laser_pos[0]-1)
-#rotate 90° to align initial laser beam with polar angle 0
-asteroid_map_r = rotate(asteroid_map)
-#asteroid_map2[rot_laser_pos] = 3
-#asteroid_map[laser_pos] = 3
+asteroid_map[laser_pos] = 3
 
 
-positions = list(np.transpose(np.nonzero(asteroid_map_r)))
-laser_idx = [x for x,pos in enumerate(positions) if all(rot_laser_pos==pos)]
+positions = list(np.transpose(np.nonzero(asteroid_map)))
+
+laser_idx = [x for x,pos in enumerate(positions) if all(laser_pos==pos)]
 positions.pop(laser_idx[0])
 
 #calculate direction vectors in polar coordinates
 sight_vectors = [cart_to_polar(pos-laser_pos) for pos in positions]
 
-#sort vectors, positions by polar angle
-sight_vectors = [x for x in sorted(sight_vectors, key=lambda pair: pair[1])]
+#sort vectors, positions by polar angle, reverse because we turn clockwise
+sight_vectors = [x for x in sorted(sight_vectors, reverse=True, key=lambda pair: pair[1])]
 
 
+#this is the lasers first shot
+laser_angle = cart_to_polar([-1,0])[1]
 
-initial_laser_angle = cart_to_polar([-1,0])
+#find asteroid closest to the laser having the same angle as initial_laser_angle
+num_vaporized = 0
+destroyed = list([0 for _ in range(len(sight_vectors))])
+while sight_vectors:
+    in_line = [ast_vector for ast_vector in sight_vectors if ast_vector[1] == laser_angle]
+    destroyed[num_vaporized] =  in_line[-1]
+    sight_vectors.remove(in_line[-1])
+    num_vaporized += 1
+    if sight_vectors:
+        laser_angle = find_new_laser_angle(laser_angle,sight_vectors)
 
-
-
-
-
-
-from numpy import exp, abs, angle
-
+destroyed_directions_cart = [polar_to_cart(x) for x in destroyed]
+destroyed_direction_cart = [[round(x) for x in direction] for direction in destroyed_directions_cart]
+destroyed_pos = [[x+y for x,y in zip(laser_pos,direction)] for direction in destroyed_direction_cart]
+dp200 = destroyed_pos[199]
+sol = dp200[1]*100+dp200[0]
+print(f"Solution for puzzle 10.2:\n Position ({dp200[1]},{dp200[0]})\nKey: {int(sol)}")
