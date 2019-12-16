cf2ee852ed8293bf3ef62ed0839a47be17dd118c
RobAnKo <robinkoch92@gmail.com>
2019-12-16 18:49:28 +0100

    finished 10.1, working on 10.2 (polar coordinates of asteroids, rotating laser)

--- AoC19/script10.py
+++ AoC19/script10.py
@@ -20,7 +20,10 @@ import scipy
 from scipy import signal
 from datetime import datetime
 import copy
-os.chdir("/home/karlchen/Desktop/AdventOfCode/AoC19")
+import math
+from typing import Iterable
+#os.chdir("/home/karlchen/Desktop/AdventOfCode/AoC19")
+os.chdir("/home/robinkoch/Desktop/AdventOfCode/AoC19")
 
 
 
@@ -42,38 +45,113 @@ def asteroid_map_from_file(inputfile):
     return(map_)
 
 
-inputfile = "Input_10_test.txt"
+
+def remove_arr_from_list(arr,list_):
+    idx = [all(arr == elem) for elem in list_]
+    ix = np.argwhere(idx)
+    list_.pop(int(ix[0]))
+    return list_
+
+
+def coincident(one, two):
+  one_hat = one / np.linalg.norm(one)
+  two_hat = two / np.linalg.norm(two)
+  dotp = np.dot(one_hat,two_hat)
+  if math.isclose(1,dotp,abs_tol=1e-09):
+      return True
+  return False
+
+
+
+
+inputfile = "Input_10_test2.txt"
 asteroid_map = asteroid_map_from_file(inputfile)
 positions = np.transpose(np.nonzero(asteroid_map))
 
-independent_per_pos = np.zeros(len(positions[0]))
+#container for number of visible asteroids per position
+visible_per_pos = np.zeros(len(positions))
+#check each asteroid
 for m,pos1 in enumerate(positions):
-    #for each asteroid, create a list of vectors to other asteroids
-    #sight_vectors = np.empty((len(positions),2))
+    print(f"checking position {m}")
+    #create a list of vectors pointing to other asteroids
     sight_vectors = [np.empty(2) for _ in range(len(positions))]
     #also how far they are away
     dists = np.empty(len(positions))
     for n,pos2 in enumerate(positions):
-        sight_vectors[n] = np.transpose(pos1-pos2)
-        dists[n] = np.linalg.norm(pos1-pos2)
+        sight_vectors[n] = np.transpose(pos2-pos1)
+        dists[n] = np.linalg.norm(pos2-pos1)
+
     #sort sight vectors from closest to farthest asteroid, then kick self-reference
-    distsort = np.argsort(dists)
     sight_vectors = [x for _, x in sorted(zip(dists,sight_vectors), key=lambda pair: pair[0])]
     sight_vectors = sight_vectors[1:]
 
-    #now gather vectors and by doing so kill those which are multiples of the gathered vector
-    sv_copy = copy.deepcopy(sight_vectors)
-    for vector1 in sv_copy:
-        #check wether vector is still available
-        #if yes, go through all the other vectors and kick them if they are multiples
-        if vector1 in sight_vectors:
-            for vector2 in sv_copy:
-                quot = vector1/vector2
-                if quot[0] == quot[1] and quot[1]>0:
-                    sight_vectors.remove(vector2)
-    independent_per_pos[m] = len(sight_vectors)
+    #now fix one vector at a time and check whether others are multiples of first vector
+    # if yes, drop them
+    ix = 0
+    while ix<len(sight_vectors):
+        #print(ix)
+        vector1 = sight_vectors[ix]
+        sv_copy = copy.deepcopy(sight_vectors)
+        sv_copy = remove_arr_from_list(vector1,sv_copy)
+        for vector2 in sv_copy:
+            if coincident(vector1,vector2):
+                sight_vectors = remove_arr_from_list(vector2,sight_vectors)
+        ix += 1
+    visible_per_pos[m] = len(sight_vectors)
+maxval = max(visible_per_pos)
+maxpos = positions[np.argmax(visible_per_pos)]
+    
+print(f"Solution for Puzzle10.1:\n Best is {maxpos} with {max(visible_per_pos)} asteroids detected")
 
-            
-    
-    #n_independent = np.linalg.matrix_rank()
-    
+
+#puzzle10.2
+
+#We know from 10.1 that the station is at [31,25]
+# if we sort the direction vectors by polar coordinates (i.e. angle) we can
+# find the next asteroid to evaporate
+
+def cart_to_polar(vector: Iterable[int]) -> Iterable[int]:
+    x = vector[0]
+    y = vector[1]
+    z = x + 1j * y
+    r = np.abs(z)#np.sqrt(x**2+y**2)
+    t = np.angle(z,deg=True)#arctan2(y,x)
+    if t<0:
+        t += 360
+    return([r,t])
+
+def rotate(matrix):
+    return np.array([x for x in zip(*matrix[::-1])])
+
+
+inputfile = "Input_10.txt"
+asteroid_map = asteroid_map_from_file(inputfile)
+laser_pos = (31,25)
+rot_laser_pos = (laser_pos[1],len(asteroid_map[0])-laser_pos[0]-1)
+#rotate 90° to align initial laser beam with polar angle 0
+asteroid_map_r = rotate(asteroid_map)
+#asteroid_map2[rot_laser_pos] = 3
+#asteroid_map[laser_pos] = 3
+
+
+positions = list(np.transpose(np.nonzero(asteroid_map_r)))
+laser_idx = [x for x,pos in enumerate(positions) if all(rot_laser_pos==pos)]
+positions.pop(laser_idx[0])
+
+#calculate direction vectors in polar coordinates
+sight_vectors = [cart_to_polar(pos-laser_pos) for pos in positions]
+
+#sort vectors, positions by polar angle
+sight_vectors = [x for x in sorted(sight_vectors, key=lambda pair: pair[1])]
+
+
+
+initial_laser_angle = cart_to_polar([-1,0])
+
+
+
+
+
+
+from numpy import exp, abs, angle
+
